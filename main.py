a157a54c7b8fdf4734d912fbf474e5140694304a
Jeffrey Wang <jeffreywang@Jeffreys-MacBook-Air.local>
2024-10-20 03:02:22 -0700

    Add character counter

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     path = "books/frankenstein.txt"
     text = get_text(path)
     word_count = get_word_count(path)
-    print(word_count)
+    char_count = count_letters(text)
+    print(char_count)
 
 
 def get_text(path):
@@ -15,6 +16,17 @@ def get_word_count(path):
     for word in word_list:
         word_count += 1
     return word_count
+
+def count_letters(text):
+    letters = {}
+    words = text.lower()
+    for word in words:
+        if word in letters:
+            letters[word] += 1
+        else:
+            letters[word] = 1
+    return letters
+
 
 main()
 
